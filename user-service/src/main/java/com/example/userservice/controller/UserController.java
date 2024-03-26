182944b6d4f561321fe7f5fccd6a3db3f7aede5d
youngdonJung <youngdon.jung@lotte.net>
2024-03-26 15:46:02 +0900

    spring jpa

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import com.netflix.discovery.converters.Auto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private Environment env;
+    private UserService userService;
+
+    @Autowired
+    private Greeting greeting;
+
+    @Autowired
+    public UserController(Environment env, UserService userService){
+        this.env = env;
+        this.userService = userService;
+    }
+
+    @GetMapping("/heath_check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody RequestUser user){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+}
